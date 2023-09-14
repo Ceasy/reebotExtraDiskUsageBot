fa91d3074b6f37b8e53d672f3e522b119330b431
ceasy <aledukar@mail.ru>
2023-09-14 18:03:14 +0700

    Изменил описание. Добавил файлы в установку

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup, Extension
 ext_modules = [Extension("main", ["main.py"])]
 
 setup(
-    name = "main",
-    ext_modules = cythonize(ext_modules)
+    name="main",
+    ext_modules=cythonize(ext_modules)
 )
